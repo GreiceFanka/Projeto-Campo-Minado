b4703df2a3433ad737dc161dda3d6f1af5454be9
GreiceFanka <greicefcardoso@gmail.com>
2023-03-16 13:11:32 +0100

    Criacao da classe PainelTabuleiro

---
+++ src/visao/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package visao;
+
+import javax.swing.JPanel;
+
+import modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
