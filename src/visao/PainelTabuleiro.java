ceaeb422ee425bf094ed3b3050b867ce88afa70f
GreiceFanka <greicefcardoso@gmail.com>
2023-03-17 13:09:38 +0100

    Implementacao do grid

--- src/visao/PainelTabuleiro.java
+++ src/visao/PainelTabuleiro.java
@@ -1,5 +1,7 @@
 package visao;
 
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 import modelo.Tabuleiro;
@@ -7,6 +9,10 @@ import modelo.Tabuleiro;
 @SuppressWarnings("serial")
 public class PainelTabuleiro extends JPanel {
 	public PainelTabuleiro(Tabuleiro tabuleiro) {
-		
+		setLayout(new GridLayout(tabuleiro.getLinhas(),tabuleiro.getColunas()));
+		tabuleiro.paraCada(c -> add(new BotaoCampo(c)));
+		tabuleiro.registrarObservador(e -> {
+			//TODO mostrar resultado pro usuario!
+		});
 	}
 }
