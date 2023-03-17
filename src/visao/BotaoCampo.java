dcc81e554c79a1e3f1828c23cc65b698775ccb6b
GreiceFanka <greicefcardoso@gmail.com>
2023-03-17 11:26:36 +0100

    Implementacao do evento do mouse e estilos

---
+++ src/visao/BotaoCampo.java
@@ -0,0 +1,90 @@
+package visao;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import modelo.Campo;
+import modelo.CampoEvento;
+import modelo.CampoObservador;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener{
+	private final Color BG_PADRAO = new Color(184,184,184);
+	private final Color BG_MARCAR = new Color(8,179,247);
+	private final Color BG_EXPLODIR = new Color(189,66,68);
+	private final Color TEXTO_VERDE = new Color(0,100,0);
+	private Campo campo;
+	
+	public BotaoCampo(Campo campo) {
+		this.campo = campo;
+		setBorder(BorderFactory.createBevelBorder(0));
+		setBackground(BG_PADRAO);
+		addMouseListener(this);
+		campo.resgistrarObservador(this);
+	}
+	
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch(evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+		default:
+			aplicarEstiloPadrao();
+		}
+	}
+	
+	private void aplicarEstiloAbrir() {
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		switch(campo.minasNaVizinhanca()) {
+		case 1:
+			setForeground(TEXTO_VERDE);
+			break;
+		case 2:
+			setForeground(Color.BLUE);
+			break;
+		case 3:
+			setForeground(Color.YELLOW);
+			break;
+		case 4:
+		case 5:
+		case 6:
+			setForeground(Color.RED);
+			break;
+		default:
+			setForeground(Color.PINK);
+		}
+		String valor = !campo.vizinhancaSegura() ? campo.minasNaVizinhanca() + "" : "";
+		setText(valor);
+	}
+	private void aplicarEstiloMarcar() {
+		
+	}
+	private void aplicarEstiloExplodir() {
+		
+	}
+	private void aplicarEstiloPadrao() {
+		
+	}
+	//Interface dos eventos do mouse
+	public void mousePressed(MouseEvent e) {
+		if(e.getButton()==1) {
+			campo.abrir();
+		}else {
+			campo.alternarMarcacao();
+		}
+	}
+	public void mouseClicked(MouseEvent e){}
+	public void mouseEntered(MouseEvent e){}
+	public void mouseExited(MouseEvent e){}
+	public void mouseReleased(MouseEvent e){}
+}
